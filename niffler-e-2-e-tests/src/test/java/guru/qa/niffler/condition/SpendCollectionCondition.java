8acc883794490598a014de69bd22f1e9b9621a93
Maverik2047 <maverik2047@yandex.ru>
2024-02-28 21:58:07 +0300

    hw12 рефактор

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/SpendCollectionCondition.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/SpendCollectionCondition.java
@@ -4,8 +4,9 @@ import com.codeborne.selenide.CheckResult;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.ex.ElementNotFound;
-import com.codeborne.selenide.ex.TextsMismatch;
+import com.codeborne.selenide.ex.MatcherError;
 import com.codeborne.selenide.impl.CollectionSource;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,7 +14,7 @@ import org.openqa.selenium.WebElement;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -22,8 +23,6 @@ public class SpendCollectionCondition {
     public static CollectionCondition spends(SpendJson... expectedSpends) {
 
         return new CollectionCondition() {
-
-            private final List<String> expectedTexts = new ArrayList<>();
 
             @Nonnull
             @Override
@@ -39,6 +38,16 @@ public class SpendCollectionCondition {
                     SpendJson expectedSpending = expectedSpends[i];
 
                     List<WebElement> cells = row.findElements(By.cssSelector("td"));
+
+                    SpendJson actualSpendInTable = new SpendJson(
+                            null,
+                            new Date(),
+                            cells.get(4).getText(),
+                            CurrencyValues.valueOf(cells.get(3).getText()),
+                            Double.valueOf(cells.get(2).getText()),
+                            cells.get(5).getText(),
+                            null
+                    );
 
                     if (!new SimpleDateFormat("dd MMM yy", Locale.ENGLISH)
                             .format(expectedSpending.spendDate()).equals(cells.get(1).getText())) {
@@ -59,7 +68,8 @@ public class SpendCollectionCondition {
                     }
                     if (!expectedSpending.description().equals(cells.get(5).getText())) {
 
-                        return CheckResult.rejected("Incorrect spends content", expectedSpending);
+                        return CheckResult.rejected("Incorrect spends content", List.of(expectedSpending,
+                                actualSpendInTable));
                     }
                 }
                 return CheckResult.accepted();
@@ -73,9 +83,8 @@ public class SpendCollectionCondition {
                     throw new ElementNotFound(collection, toString(), timeoutMs, cause);
                 }
 
-                String message = lastCheckResult.getMessageOrElse(() -> "Texts mismatch");
-
-                throw new TextsMismatch(message, collection, expectedTexts, actualTexts, explanation, timeoutMs, cause);
+                throw new MatcherError(explanation, ((List) lastCheckResult.getActualValue()).get(0).toString(),
+                        ((List) lastCheckResult.getActualValue()).get(1).toString(), collection, cause, timeoutMs);
             }
 
             @Override
